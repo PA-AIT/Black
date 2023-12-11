3db781c2e35001d64ca49a54fd285d839230785b
PA-AIT <153344795+PA-AIT@users.noreply.github.com>
2023-12-11 10:40:50 +0530

    Update White.py

--- White.py
+++ White.py
@@ -6,31 +6,46 @@ from io import BytesIO
 import fitz  # PyMuPDF
 import streamlit as st
 from transformers import T5ForConditionalGeneration, T5Tokenizer
-import sentencepiece  # Add this import
+import sentencepiece
+from concurrent.futures import ThreadPoolExecutor
 
 # Download NLTK resources
 nltk.download('punkt')
 
 # Streamlit app title
 st.title("Automate2PDF: Simplified Data Transfer")
+
 # Create input fields for the user, password, and email address
 user = st.text_input("Enter your email address")
 password = st.text_input("Enter your email password", type="password")
 pdf_email_address = st.text_input("Enter the email address from which to extract PDFs")
 selected_date = st.text_input("Enter the date (YYYY-MM-DD) to filter emails")
 
-# Function to extract text from PDF using PyMuPDF
-def extract_text_from_pdf(pdf_bytes):
+# Initialize the T5 model and tokenizer outside the loop
+model = T5ForConditionalGeneration.from_pretrained('t5-small')
+tokenizer = T5Tokenizer.from_pretrained('t5-small')
+
+# Function to extract chapters from PDF using PyMuPDF
+def extract_chapters_from_pdf(pdf_bytes):
     pdf_document = fitz.open(stream=pdf_bytes, filetype="pdf")
-    text = ""
+    chapters = []
+
     for page_num in range(pdf_document.page_count):
-        text += pdf_document[page_num].get_text()
-    return text
+        page_text = pdf_document[page_num].get_text()
+
+        # Assuming chapters are separated by a keyword, adjust this based on your document structure
+        chapter_start_keywords = ["Chapter", "CHAPTER", "Section", "SECTION"]
+        
+        for start_keyword in chapter_start_keywords:
+            if start_keyword in page_text:
+                # Split the text at the chapter start keyword
+                chapter_text = page_text.split(start_keyword, 1)[-1]
+                chapters.append(chapter_text)
+
+    return chapters
 
 # Function to summarize text using T5 model
 def summarize_text_t5(text):
-    model = T5ForConditionalGeneration.from_pretrained('t5-small')
-    tokenizer = T5Tokenizer.from_pretrained('t5-small')
     inputs = tokenizer.encode("summarize: " + text, return_tensors="pt", max_length=512, truncation=True)
     summary_ids = model.generate(inputs, max_length=150, min_length=30, length_penalty=2.0, num_beams=4, early_stopping=True)
     summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
@@ -65,8 +80,9 @@ if st.button("Fetch and Display PDF Summaries"):
 
             info_list = []
 
-            # Iterate through messages
-            for num in mail_id_list:
+            # Function to process each email
+            def process_email(num):
+                nonlocal info_list
                 typ, data = my_mail.fetch(num, '(RFC822)')
                 msg = email.message_from_bytes(data[0][1])
 
@@ -75,24 +91,29 @@ if st.button("Fetch and Display PDF Summaries"):
                         # Extract email date
                         email_date = msg["Date"]
 
-                        # Extract text from PDF using PyMuPDF
+                        # Extract chapters from PDF using PyMuPDF
                         pdf_bytes = part.get_payload(decode=True)
-                        pdf_text = extract_text_from_pdf(pdf_bytes)
+                        chapters = extract_chapters_from_pdf(pdf_bytes)
 
-                        # Summarize the PDF content using T5 model
-                        summary = summarize_text_t5(pdf_text)
+                        for chapter_num, chapter_text in enumerate(chapters, start=1):
+                            # Summarize each chapter using T5 model
+                            summary = summarize_text_t5(chapter_text)
 
-                        info = {"Summarized Content": summary, "Received Date": email_date}
-                        info_list.append(info)
+                            info = {"Summarized Content": summary, "Received Date": email_date, "Chapter": chapter_num}
+                            info_list.append(info)
+
+            # Parallelize the processing of emails
+            with ThreadPoolExecutor() as executor:
+                executor.map(process_email, mail_id_list)
 
             # Display the summarized content
             for info in info_list:
-                st.subheader(f"Received Date: {info['Received Date']}")
+                st.subheader(f"Chapter {info['Chapter']} - Received Date: {info['Received Date']}")
                 st.write(info["Summarized Content"])
 
             # Download button
             if st.button("Download Summaries as Text File"):
-                summary_text = "\n\n".join(f"Received Date: {info['Received Date']}\n{info['Summarized Content']}" for info in info_list)
+                summary_text = "\n\n".join(f"Chapter {info['Chapter']} - Received Date: {info['Received Date']}\n{info['Summarized Content']}" for info in info_list)
                 st.download_button(
                     label="Download Summaries",
                     data=summary_text,
